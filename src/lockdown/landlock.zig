45bf4e610ac5b0e1a4b597e960c289363d0f7d84
Jan200101 <sentrycraft123@gmail.com>
2025-12-09 09:33:25 +0100

    limit program, access to reading

--- src/lockdown/landlock.zig
+++ src/lockdown/landlock.zig
@@ -29,6 +29,52 @@ const landlock_access_fs = packed struct(u64) {
     TRUNCATE: bool = false,
     IOCTL_DEV: bool = false,
     reserved: u48 = 0,
+
+    const Self = @This();
+
+    fn all() !Self {
+        const abi = try landlock_create_ruleset(null, 0, .{ .VERSION = true });
+
+        var fs_access: Self = .{
+            .EXECUTE = true,
+            .WRITE_FILE = true,
+            .READ_FILE = true,
+            .READ_DIR = true,
+            .REMOVE_DIR = true,
+            .REMOVE_FILE = true,
+            .MAKE_CHAR = true,
+            .MAKE_DIR = true,
+            .MAKE_REG = true,
+            .MAKE_SOCK = true,
+            .MAKE_FIFO = true,
+            .MAKE_BLOCK = true,
+            .MAKE_SYM = true,
+            .REFER = true,
+            .TRUNCATE = true,
+            .IOCTL_DEV = true,
+        };
+
+        sw: switch (abi) {
+            1 => {
+                fs_access.REFER = false;
+                continue :sw 2;
+            },
+            2 => {
+                fs_access.TRUNCATE = false;
+                continue :sw 4;
+            },
+            4 => {
+                fs_access.IOCTL_DEV = false;
+                continue :sw 5;
+            },
+            5 => {
+                fs_access.IOCTL_DEV = false;
+            },
+            else => {},
+        }
+
+        return fs_access;
+    }
 };
 
 const landlock_rule_type = enum(u32) {
@@ -97,32 +143,19 @@ fn landlock_restrict_self(
 }
 
 pub fn lockdown_dir(dir: std.fs.Dir) !void {
-    const access: landlock_access_fs = .{
-        .EXECUTE = true,
-        .WRITE_FILE = true,
-        .READ_FILE = true,
-        .READ_DIR = true,
-        .REMOVE_DIR = true,
-        .REMOVE_FILE = true,
-        .MAKE_CHAR = true,
-        .MAKE_DIR = true,
-        .MAKE_REG = true,
-        .MAKE_SOCK = true,
-        .MAKE_FIFO = true,
-        .MAKE_BLOCK = true,
-        .MAKE_SYM = true,
-    };
-
-    _ = try landlock_create_ruleset(null, 0, .{ .VERSION = true });
+    const fs_access: landlock_access_fs = try .all();
 
     var ruleset: landlock_ruleset_attr = .{
-        .handled_access_fs = access,
+        .handled_access_fs = fs_access,
     };
     const ruleset_fd = try landlock_create_ruleset(&ruleset, @sizeOf(landlock_ruleset_attr), .{});
     defer posix.close(ruleset_fd);
 
     var path_rule: landlock_path_beneath_attr = .{
-        .allowed_access = access,
+        .allowed_access = .{
+            .READ_FILE = true,
+            .READ_DIR = true,
+        },
         .parent_fd = dir.fd,
     };
     try landlock_add_rule(ruleset_fd, .PATH_BENEATH, &path_rule, 0);
